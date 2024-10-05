fddaa42f15ff9ce5b62a2c1c515f3f20f6cc2a52
Sandesh Kawane <sandeshkawane21@gmail.com>
2024-10-05 12:58:24 +0530

    Commit

--- final_project/index.js
+++ final_project/index.js
@@ -1,22 +1,30 @@
 const express = require('express');
 const jwt = require('jsonwebtoken');
-const session = require('express-session')
+const session = require('express-session');
 const customer_routes = require('./router/auth_users.js').authenticated;
 const genl_routes = require('./router/general.js').general;
 
 const app = express();
-
 app.use(express.json());
 
-app.use("/customer",session({secret:"fingerprint_customer",resave: true, saveUninitialized: true}))
+// Setup session management
+app.use("/customer", session({ secret: "fingerprint_customer", resave: true, saveUninitialized: true }));
 
-app.use("/customer/auth/*", function auth(req,res,next){
-//Write the authenication mechanism here
+// Token verification middleware
+app.use("/customer/auth/*", (req, res, next) => {
+    const authHeader = req.headers['authorization'];
+    const token = authHeader && authHeader.split(' ')[1];
+    if (token == null) return res.sendStatus(401);
+
+    jwt.verify(token, '1234567890', (err, user) => {
+        if (err) return res.sendStatus(403);
+        req.username = user.username;
+        next();
+    });
 });
- 
-const PORT =5000;
 
+const PORT = 5000;
 app.use("/customer", customer_routes);
 app.use("/", genl_routes);
 
-app.listen(PORT,()=>console.log("Server is running"));
+app.listen(PORT, () => console.log(`Server is running on port ${PORT}`));
